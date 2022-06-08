dfd5531fdb32cbfb4c3f82b87a1f368355c51991
Golden abdu <abdulrahman.goldendawn@gmail.com>
2022-06-08 10:17:34 +0600

    separated the UI logic to a separate class called board

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-import copy
 import itertools
 from random import randint
 from typing import List
 
 import emoji
 
-from helpers import index_in_list
-
-PASSENGER_ID = 1
+from helpers import index_in_list, bcolors
 
 
 class Building:
@@ -85,225 +82,26 @@ class Floor:
         return f"F{self.floor_num}"
 
 
-class Elevator:
-    def __init__(self, building: Building) -> None:
-        self.building: Building = building
-        self.building_floors: int = self.building.floors
-        self.next_passenger: int = 0
-        self.max_floor: int = 0
-        self.min_floor: int = 0
-        self.current_floor: int = 0
-        self.has_button_up: bool = True
-        self.has_button_down: bool = False
-        self.going_up = True
-        self.going_down = False
-        self.direct = "down"
-        self.steps: int = 0
-        self.passengers_count: int = 0
-        self.elev_passengers: List[Passenger] = []
-        self.left_elev_passengers: List[Passenger] = []
-        self.entered_passengers: List[Passenger] = []
-
-    def manage_entering_passengers(self, floor: Floor, current_floor: int):
-        newly_entered_passengers = []
-
-        floor_passengers = floor.passengers
-
-        for passenger in floor_passengers:
-            # check whether the elevator is going up or down
-            if self.direct == "up":
-                if passenger.arrival_floor > current_floor and not (
-                    passenger.arrival_floor == current_floor
-                ):
-                    self.add_passenger(passenger)
-                    newly_entered_passengers.append(passenger)
-
-            elif self.direct == "down":
-                if passenger.arrival_floor < current_floor and not (
-                    passenger.arrival_floor == current_floor
-                ):
-                    self.add_passenger(passenger)
-                    newly_entered_passengers.append(passenger)
-
-        # remove the passenger so we don't have any dupliction in the
-        # next iteration
-        for entered_pass in newly_entered_passengers:
-            if entered_pass in self.elev_passengers:
-                floor_passengers.remove(entered_pass)
-
-    def manage_leaving_passengers(self, passengers: List[Passenger]):
-        passenegers = []
-
-        for passenger in self.elev_passengers:
-            if passenger.arrival_floor == self.current_floor:
-                passenegers.append(passenger)
-                self.remove_passenger(passenger)
-
-        self.left_elev_passengers = passenegers
-
-        # remove the passenger so we don't have any dupliction in the
-        # next iteration
-        for leaving_pass in passenegers:
-            self.elev_passengers.remove(leaving_pass)
-
-    def add_passenger(self, passenger: Passenger):
-        if not self.passengers_count >= 5:
-            self.elev_passengers.append(passenger)
-            self.passengers_count += 1
-
-            # update max_floor or min_floor if it's not bigger than the
-            # passenger' arrival floor
-            if self.direct == "up":
-                if not self.max_floor > passenger.arrival_floor:
-                    self.max_floor = passenger.arrival_floor
-            elif self.direct == "down":
-                if not self.min_floor > passenger.arrival_floor:
-                    self.min_floor = passenger.arrival_floor
-
-    def remove_passenger(self, passenger: Passenger):
-        print(
-            f"Passenger {passenger.id} arrived at {passenger.arrival_floor} floor"
-        )
-        self.passengers_count -= 1
-
-    def generate_floors(self) -> List[Floor]:
-        floors_list = []
-
-        building_floor = self.building.floors
-
-        for floor_num in range(1, building_floor):
-            floors_list.append(
-                Floor(floor_num, building_floor, self.building.counter)
-            )
-
-        return floors_list
-
-    def SetUp(self, floor: Floor):
-        self.current_floor = floor.floor_num
-
-    def move_up(
-        self,
-        floors: List[Floor],
-        start_floor: Floor = None,
-        is_reversed: bool = False,
-    ):
-        print("*" * 30)
-        print("Elevator is moving Up")
-        print("*" * 30)
-
-        self.direct = "up"
-
-        if not is_reversed:
-            floors = sorted(
-                floors
-                if not start_floor
-                else floors[: floors.index(start_floor)],
-                key=lambda f: f.floor_num,
-            )
-
-        for floor in floors:
-            self.SetUp(floor)
-            self.manage_entering_passengers(floor, floor.floor_num)
-            self.manage_leaving_passengers(self.elev_passengers)
-
-            prev_floor = (
-                None
-                if not index_in_list(floors, floor.floor_num - 2)
-                else floors[floor.floor_num - 2]
-            )
-            next_floor = (
-                None
-                if not index_in_list(floors, floor.floor_num)
-                else floors[floor.floor_num]
-            )
-
-            if not self.is_elevator_empty:
-                self.display(floor, prev_floor, next_floor)
-
-        self.direct = "down"
-
-    def move_down(
-        self,
-        floors: List[Floor],
-        start_floor: Floor = None,
-        is_reversed: bool = False,
-    ):
-        print("*" * 30)
-        print("Elevator is moving Down")
-        print("*" * 30)
-
-        self.direct = "down"
-
-        reversed_floors = sorted(
-            floors if not start_floor else floors[: floors.index(start_floor)],
-            key=lambda f: f.floor_num,
-            reverse=True,
-        )
-
-        for floor in reversed_floors:
-            self.SetUp(floor)
-            self.manage_entering_passengers(floor, floor.floor_num)
-            self.manage_leaving_passengers(self.elev_passengers)
-
-            prev_floor = (
-                None
-                if not index_in_list(floors, floor.floor_num - 2)
-                else floors[floor.floor_num - 2]
-            )
-            next_floor = (
-                None
-                if not index_in_list(floors, floor.floor_num)
-                else floors[floor.floor_num]
-            )
-
-            if not self.is_elevator_empty:
-                self.display(floor, prev_floor, next_floor)
-
-        self.direct = "up"
-
-    def run(self):
-        floors = self.generate_floors()
-        self.display_floors_passenegers(floors)
-        self.move_down(floors)
-
-        while self.is_passengers_exists(floors):
-            if self.direct == "up":
-                self.move_up(floors)
-            elif self.direct == "down":
-                self.move_down(floors)
-
-    @property
-    def is_elevator_empty(self):
-        return len(self.elev_passengers) == 0
-
-    def is_passengers_exists(self, floors: List[Floor]):
-        for floor in floors:
-            if len(floor.passengers) > 0:
-                return True
-
-        return False
-
-    def find_nearest_floor_with_passengers(
-        self, floor: Floor, floors: List[Floor]
-    ):
-        floor_indx = floors.index(floor)
-
-    ############### Program UI ###############
+class Board:
     def display(
         self,
         floor: Floor,
+        elev_passengers: List[Passenger],
         previous_floor: Floor = None,
         next_floor: Floor = None,
+        direct: str = "up",
     ):
+
         emoji_string = "  ".join(
             [
                 emoji.emojize(":detective: ", language="en") + p.__str__()
-                for p in self.elev_passengers
+                for p in elev_passengers
                 if not (
-                    p.arrival_floor == 1 and p.floor_num == self.current_floor
+                    p.arrival_floor == 1 and p.floor_num == floor.floor_num
                 )
             ]
         )
+
         prev_floor_emoji_string = (
             ""
             if not previous_floor
@@ -354,7 +152,7 @@ class Elevator:
 
         direction_emoji = (
             emoji.emojize(":up_arrow: ", language="en")
-            if self.direct == "up"
+            if direct == "up"
             else emoji.emojize(":down_arrow: ", language="en")
         )
 
@@ -365,7 +163,7 @@ class Elevator:
         print("...")
         print(footer)
 
-        if previous_floor and previous_floor.floor_num < self.current_floor:
+        if previous_floor and previous_floor.floor_num < floor.floor_num:
             print(f"{fins}{dashes_string}")
             print(
                 f"{fins}{previous_floor}{fins} ",
@@ -398,7 +196,7 @@ class Elevator:
             ),
         )
 
-        if next_floor and self.current_floor != 1:
+        if next_floor and floor.floor_num != 1:
             print(f"{fins}{dashes_string}")
             print(
                 f"{fins}{next_floor}{fins} ",
@@ -495,19 +293,245 @@ class Elevator:
         )
         print()
 
-    def display_floors_passenegers(self, floors: List[Floor]):
+    def display_elevator_meta_data_passenegers(self, floors: List[Floor]):
         print("*" * 20)
         print("PASSENGERS IN EACH FLOOR")
         print("*" * 20)
 
         for floor in floors:
             print(floor, " : ", end=" ")
-            [print(p, end=" | ") for p in floor.passengers]
+            [
+                print(emoji.emojize(":detective:", language="en"), end=" | ")
+                for p in floor.passengers
+            ]
             print()
+
+    def pop_up_text(self, text):
+        print("*" * 30)
+        print(bcolors.OKCYAN, text, bcolors.ENDC)
+        print("*" * 30)
+
+    def inline_text(self, text: str, color: str = None):
+        if color == "red":
+            print(bcolors.FAIL, text, bcolors.ENDC)
+        elif color == "green":
+            print(bcolors.OKGREEN, text, bcolors.ENDC)
+        else:
+            print(text)
+
+
+class Elevator:
+    def __init__(self, building: Building, board: Board) -> None:
+        self.building: Building = building
+        self.board: Board = board
+        self.max_floor: int = 0
+        self.min_floor: int = 0
+        self.current_floor: int = 0
+        self.direct = "up"
+        self.passengers_count: int = 0
+        self.elev_passengers: List[Passenger] = []
+        self.left_elev_passengers: List[Passenger] = []
+        self.entered_passengers: List[Passenger] = []
+
+    def manage_entering_passengers(self, floor: Floor, current_floor: int):
+        newly_entered_passengers = []
+
+        floor_passengers = floor.passengers
+
+        for passenger in floor_passengers:
+            # check whether the elevator is going up or down
+            if self.direct == "up":
+                if passenger.arrival_floor > current_floor and not (
+                    passenger.arrival_floor == current_floor
+                ):
+                    self.add_passenger(passenger)
+                    newly_entered_passengers.append(passenger)
+
+            elif self.direct == "down":
+                if passenger.arrival_floor < current_floor and not (
+                    passenger.arrival_floor == current_floor
+                ):
+                    self.add_passenger(passenger)
+                    newly_entered_passengers.append(passenger)
+
+        # remove the passenger so we don't have any dupliction in the
+        # next iteration
+        for entered_pass in newly_entered_passengers:
+            if entered_pass in self.elev_passengers:
+                floor_passengers.remove(entered_pass)
+
+    def manage_leaving_passengers(self, passengers: List[Passenger]):
+        passenegers = []
+
+        for passenger in self.elev_passengers:
+            if passenger.arrival_floor == self.current_floor:
+                passenegers.append(passenger)
+                self.remove_passenger(passenger)
+
+        self.left_elev_passengers = passenegers
+
+        # remove the passenger so we don't have any dupliction in the
+        # next iteration
+        for leaving_pass in passenegers:
+            self.elev_passengers.remove(leaving_pass)
+
+    def add_passenger(self, passenger: Passenger):
+        if not self.passengers_count >= 5:
+            self.elev_passengers.append(passenger)
+            self.passengers_count += 1
+
+            # update max_floor or min_floor if it's not bigger than the
+            # passenger' arrival floor
+            if self.direct == "up":
+                if not self.max_floor > passenger.arrival_floor:
+                    self.max_floor = passenger.arrival_floor
+            elif self.direct == "down":
+                if not self.min_floor > passenger.arrival_floor:
+                    self.min_floor = passenger.arrival_floor
+
+    def remove_passenger(self, passenger: Passenger):
+        self.board.inline_text(
+            f"Passenger {passenger.id} arrived at {passenger.arrival_floor} floor",
+            color="red",
+        )
+        self.passengers_count -= 1
+
+    def generate_floors(self) -> List[Floor]:
+        floors_list = []
+
+        building_floor = self.building.floors
+
+        for floor_num in range(1, building_floor):
+            floors_list.append(
+                Floor(floor_num, building_floor, self.building.counter)
+            )
+
+        return floors_list
+
+    def SetUp(self, floor: Floor):
+        self.current_floor = floor.floor_num
+
+    def move_up(
+        self,
+        floors: List[Floor],
+        start_floor: Floor = None,
+        is_reversed: bool = False,
+    ):
+        self.board.pop_up_text("Elevator is moving Up")
+
+        self.direct = "up"
+
+        if not is_reversed:
+            floors = sorted(
+                floors
+                if not start_floor
+                else floors[: floors.index(start_floor)],
+                key=lambda f: f.floor_num,
+            )
+
+        for floor in floors:
+            self.SetUp(floor)
+            self.manage_entering_passengers(floor, floor.floor_num)
+            self.manage_leaving_passengers(self.elev_passengers)
+
+            prev_floor = (
+                None
+                if not index_in_list(floors, floor.floor_num - 2)
+                else floors[floor.floor_num - 2]
+            )
+            next_floor = (
+                None
+                if not index_in_list(floors, floor.floor_num)
+                else floors[floor.floor_num]
+            )
+
+            if not self.is_elevator_empty:
+                self.board.display(
+                    floor,
+                    self.elev_passengers,
+                    prev_floor,
+                    next_floor,
+                    self.direct,
+                )
+
+        self.direct = "down"
+
+    def move_down(
+        self,
+        floors: List[Floor],
+        start_floor: Floor = None,
+        is_reversed: bool = False,
+    ):
+        self.board.pop_up_text("Elevator is moving Down")
+
+        self.direct = "down"
+
+        reversed_floors = sorted(
+            floors if not start_floor else floors[: floors.index(start_floor)],
+            key=lambda f: f.floor_num,
+            reverse=True,
+        )
+
+        for floor in reversed_floors:
+            self.SetUp(floor)
+            self.manage_entering_passengers(floor, floor.floor_num)
+            self.manage_leaving_passengers(self.elev_passengers)
+
+            prev_floor = (
+                None
+                if not index_in_list(floors, floor.floor_num - 2)
+                else floors[floor.floor_num - 2]
+            )
+            next_floor = (
+                None
+                if not index_in_list(floors, floor.floor_num)
+                else floors[floor.floor_num]
+            )
+
+            if not self.is_elevator_empty:
+                self.board.display(
+                    floor,
+                    self.elev_passengers,
+                    prev_floor,
+                    next_floor,
+                    self.direct,
+                )
+
+        self.direct = "up"
+
+    def is_passengers_exists(self, floors: List[Floor]):
+        for floor in floors:
+            if len(floor.passengers) > 0:
+                return True
+
+        return False
+
+    def find_nearest_floor_with_passengers(
+        self, floor: Floor, floors: List[Floor]
+    ):
+        floor_indx = floors.index(floor)
+
+    def run(self):
+        """
+        Runs the elevator until there's no 
+        """
+        floors = self.generate_floors()
+        self.board.display_elevator_meta_data_passenegers(floors)
+
+        while self.is_passengers_exists(floors):
+            if self.direct == "up":
+                self.move_up(floors)
+            elif self.direct == "down":
+                self.move_down(floors)
+
+    @property
+    def is_elevator_empty(self):
+        return len(self.elev_passengers) == 0
 
 
 if "__main__" == __name__:
     counter = itertools.count(start=1)
+    board = Board()
     building = Building(counter)
-    elevator = Elevator(building)
+    elevator = Elevator(building, board)
     elevator.run()
